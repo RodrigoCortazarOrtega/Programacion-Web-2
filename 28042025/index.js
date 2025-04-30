a3a9a822effeaafdcb2660a250f64324ebe19e82
RodrigoCortazarOrtega <rodrigo.cortazar463@gmail.com>
2025-04-29 23:43:16 -0600

    Server Node

--- 28042025/index.js
+++ 28042025/index.js
@@ -1,20 +1,8 @@
-//Importamos el modulo de http incluido en Node.js
-const serverCreadoHTTTP=require("node:http");
-//Definimos el puerto donde va a tener salida nuestro servidor
-const puertoSalida=4038;
-//Creamos el sevidor
-const servidorCreado=serverCreadoHTTTP.createServer((req,res)=>{
-    //Establece el encabezado de respuesta
-    res.writeHead(200,{'Content-Type':"text/plain"});
-    res.end("Hola mundo desde Node.js con el modulo http");
+const puerto=49201;
+const http=require("node:http");
+const server=http.createServer((req,res)=>{
+    res.writeHead(200, { 'Content-Type': 'text/plain' }); // Encabezado OK, tipo texto
+    res.end('Pagina en progreso'); // Respuesta final al cliente
 });
-servidorCreado.listen(puertoSalida,()=>{
-    console.log("Sevidor en escucha desde http://127.0.0.1:"+puertoSalida);
-});
-//Con middleware haremos las peticiones, registros ,...
 
-
-
-
-
-//De tarea: Generar una replica de esto funcional
+server.listen(puerto,console.log("Sevidor en escucha desde http://127.0.0.1:"+puerto));
